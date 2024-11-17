d057af771f496dc849048a50852d5f094cb958ce
maksklimko <maks.klmqaz@gmail.com>
2024-11-17 22:08:38 +0200

    Setup firebase

--- provider_todo_list/lib/main.dart
+++ provider_todo_list/lib/main.dart
@@ -1,20 +1,28 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider_todo_list/common/theme/app_theme.dart';
 import 'package:provider_todo_list/routes.dart';
 
 import 'features/todo_list/screens/main_screen/main_screen.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Todo list',
-      initialRoute: '/',
+      initialRoute: Routes.mainScreen,
       routes: Routes.routes,
       theme: AppTheme.lightTheme,
       darkTheme: AppTheme.darkTheme,
