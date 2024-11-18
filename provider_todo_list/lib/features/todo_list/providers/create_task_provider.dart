67db96c22a48e8394e6ea71b520ee3153ab7522b
maksklimko <maks.klmqaz@gmail.com>
2024-11-18 19:08:30 +0200

    Added title validation

--- provider_todo_list/lib/features/todo_list/providers/create_task_provider.dart
+++ provider_todo_list/lib/features/todo_list/providers/create_task_provider.dart
@@ -4,11 +4,13 @@ import 'package:provider_todo_list/features/todo_list/models/task.dart';
 
 class CreateTaskProvider with ChangeNotifier {
   final TextEditingController taskTitleController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
 
   Future createTask() async {
     await TasksRepository.addTask(
       Task(title: taskTitleController.text),
     );
+
     clearTaskTitle();
   }
 
--- provider_todo_list/lib/features/todo_list/screens/create_item_screen/create_item_screen.dart
+++ provider_todo_list/lib/features/todo_list/screens/create_item_screen/create_item_screen.dart
@@ -12,37 +12,55 @@ class CreateItemScreen extends StatelessWidget {
       body: ListenableBuilder(
         listenable: model,
         builder: (ctx, child) {
-          return Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 40),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Spacer(flex: 5,),
-                Padding(
-                  padding: const EdgeInsets.only(left: 20),
-                  child: Text(
-                    "Title",
-                    style: Theme
-                        .of(context)
-                        .textTheme
-                        .titleMedium,
+          return Form(
+            key: model.formKey,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 40),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Spacer(
+                    flex: 5,
                   ),
-                ),
-                TextField(controller: model.taskTitleController,),
-                Spacer(),
-                SizedBox(
-                  width: double.infinity,
-                  height: 50,
-                  child: ElevatedButton(
-                    onPressed: ()async{
-                      model.createTask();
-                      Navigator.pop(context);
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20),
+                    child: Text(
+                      "Title",
+                      style: Theme.of(context).textTheme.titleMedium,
+                    ),
+                  ),
+                  TextFormField(
+                    controller: model.taskTitleController,
+                    validator: (value) {
+                      if (value?.isEmpty ?? true) {
+                        return 'Please enter a title';
+                      }
+                      if ((value?.length ?? 0) < 3) {
+                        return 'Title is too short';
+                      }
+                      return null;
                     },
-                    child: Text("Continue"),
                   ),
-                ),
-                Spacer(flex: 5,),
-              ],
+                  Spacer(),
+                  SizedBox(
+                    width: double.infinity,
+                    height: 50,
+                    child: ElevatedButton(
+                      onPressed: () async {
+                        final isValid =
+                            (model.formKey.currentState?.validate() ?? false);
+                        if (!isValid) return;
+                        model.createTask();
+                        Navigator.pop(context);
+                      },
+                      child: Text("Continue"),
+                    ),
+                  ),
+                  Spacer(
+                    flex: 5,
+                  ),
+                ],
+              ),
             ),
           );
         },
